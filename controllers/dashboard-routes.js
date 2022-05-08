9e77f264e5489971f4229adb944ae2fe89d09acc
YiLin Ong <chauntelleinc@gmail.com>
2022-05-08 13:37:58 -0400

    GET route to edit specific ID, if user is logged in, find that post by the id

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// GET route to find all user datas, comments made my users on dashboard page
+// GET route to find all user datas, comments made by users on dashboard page
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
         where: {
@@ -39,3 +39,46 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+// when a user clicks to edit a specific id, making sure they are loggedIn
+router.get('/edit/:id', withAuth, (req, res) => {
+    // if user is loggedIn, find that post by the id
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id',
+        'title',
+        'content',
+        'created_at'
+    ],
+    include: [{
+        model: User,
+        attributes: ['username']
+    },
+    {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+            model: User,
+            attributes: ['username']
+        }
+    }
+]
+    })
+    // if no post is found with that data found by id, post error message
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id. '});
+            return;
+        }
+        // or else return object rendered to edit-post page
+        const post = dbPostData.get({ plain: true });
+        res.render('edit-post', { post, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//when a user clicks /new to add a post, render that page
