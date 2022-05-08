dcdace345c0dc97ccc412eff9b7e20ecef673f32
YiLin Ong <chauntelleinc@gmail.com>
2022-05-08 13:42:15 -0400

    added GET routes for login and signup page

---
+++ controllers/home-routes.js
@@ -0,0 +1,108 @@
+// imports our connections
+const sequelize = require('../config/connection');
+// imports our 3 model object files
+const { Post, User, Comment } = require('../models');
+// imports express' router object
+const router = require('express').Router();
+
+// GET route on home page, find all posts
+router.get('/', (req, res) => {
+// read the whole table from the db with the findAll method
+    Post.findAll({
+// to select only some attributes, use the attributes option
+// most often, you pass an array
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [{
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    // map response to remove extra Sequelize model crap
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            console.log('------------------------------------------');
+            console.log(posts);
+            console.log('------------------------------------------');
+            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// GET route to login page. if login button is clicked, redirect to login handlebars page
+// if user is on route and logged in, redirect to home page
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+// GET route to signup page, 
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// when a user clicks on a post on the home page, view it
+// eventually this will be where a user can add comments
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+            console.log(post);
+            res.render('single-post', { post, loggedIn: req.session.loggedIn });
+
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
