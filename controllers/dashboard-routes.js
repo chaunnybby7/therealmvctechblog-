7f6f2a553e4356d0904cf4390ccc12dc0a6eacf0
YiLin Ong <chauntelleinc@gmail.com>
2022-05-08 13:30:53 -0400

    GET route to find all user datas, comments made by users on dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,3 +3,39 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+// GET route to find all user datas, comments made my users on dashboard page
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [{
+            model: Comment, 
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            icnlude: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
+})
+    .then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true}));
+    res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
