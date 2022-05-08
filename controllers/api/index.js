7dca3bbb52fa09bd9edbd0d122800a01373d4f56
YiLin Ong <chauntelleinc@gmail.com>
2022-05-07 23:01:05 -0400

    added code to index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+// import user, post and comment routes 
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
+const { post } = require('./comment-routes.js');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router; 
