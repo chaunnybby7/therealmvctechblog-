7f66477f7617ca411545f912a92e3661302eb4fc
YiLin Ong <chauntelleinc@gmail.com>
2022-05-09 23:49:02 -0400

    added first and second comment_text

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,12 @@
+const { Comment } = require('../models');
+
+const commentData = [{
+    comment_text: "I want to succeed in coding.",
+    user_id: 1,
+    post_id: 1
+},
+{
+    comment_text: "I can't give up even if they force me to.",
+    user_id: 2,
+    post_id: 2
+},
