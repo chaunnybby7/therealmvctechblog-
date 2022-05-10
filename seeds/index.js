4c7f6384cdd0600d2dfa4706b62cddc995de93c3
YiLin Ong <chauntelleinc@gmail.com>
2022-05-10 00:03:40 -0400

    async() to seed data from these files

--- seeds/index.js
+++ seeds/index.js
@@ -4,3 +4,12 @@ const seedComments = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
 
+const seedAll = async() => {
+    await sequelize.sync({ force: true });
+    await seedUsers();
+    await seedPosts();
+    await seedComments();
+    process.exit(0);
+};
+
+seedAll;
